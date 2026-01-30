bf7e4e491d3dad6503c739759eb906214116a53a
Chetan Prajapat <chetanprajapat8800@gmail.com>
2026-01-30 13:28:09 +0530

    Add files via upload

---
+++ srinng3.py
@@ -0,0 +1,28 @@
+# name = input("enter your name")
+
+# print(f"Good Afternoon {name}")
+
+
+
+
+# latter = '''Dear <|name|>,
+#           You are selected!,
+#           <|Date|>'''
+
+# print(latter.replace("<|name|>", "Chetan").replace("<|Date|>", "24 sept"))
+
+
+# name = "Chetan is a good  boy"
+
+# a = name.find("  ")
+# print(a)
+       
+# name = "Chetan  prajapat"
+# print(name.replace("  ", " "))
+
+
+# letter = "Dear Harry , \n\tthis pyhton course is nice, \nthank! " 
+# print(letter)
+
+a ="Chetan"
+print(a[2])
